517af4ad5c042f429d7a1ce57bb5287e47298f39
gomshiki <gomshiki@gmail.com>
2022-06-27 21:45:08 +0900

    초기 project commit - 김준성

---
+++ src/main/java/gomshiki/singAgain/SingAgainApplication.java
@@ -0,0 +1,13 @@
+package gomshiki.singAgain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingAgainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingAgainApplication.class, args);
+	}
+
+}
